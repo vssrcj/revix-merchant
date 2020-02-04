168e4fea53b7734b3930ead3fc8caa91453710a5
CJ Visser <cj@bluebird.dev>
2020-02-04 19:05:07 +0200

    Header init.

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,45 +7,37 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
+import { ThemeProvider } from 'styled-components';
 
 import Header from './header';
-import './layout.css';
+import GlobalStyle from '../styles/global-style';
+import theme from '../styles/theme';
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0 1.0875rem 1.45rem',
-        }}
-      >
+const Layout = ({ children }) => (
+  <>
+    <GlobalStyle />
+    <Header />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0 1.0875rem 1.45rem',
+      }}
+    >
+      <ThemeProvider theme={theme}>
         <main>{children}</main>
-        <footer>
-          ©
-          {' '}
-          {new Date().getFullYear()}
-          , Built with
-          {' '}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  );
-};
+      </ThemeProvider>
+      <footer>
+        ©
+        {' '}
+        {new Date().getFullYear()}
+        , Built with
+        {' '}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
+  </>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
